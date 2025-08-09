28580c89531b4d3ce9a65503c9b046cda1206969
Arya Wiratama <aryawiratama2401@gmail.com>
2025-08-09 20:45:14 +0700

    update: sistem persamaan linear

---
+++ learn/sistem_persamaan_linear.py
@@ -0,0 +1,27 @@
+'''
+
+Persamaan linear adalah persamaan dimana perubahnya tidak membuat eksponensial, 
+trigonometri (seperti sin, cos, dll), perkalian, pembagian dengan perubah lain atau dirinya sendiri
+
+Bisa ditulis dalam bentuk ax = b
+
+dimana 
+a dinamakan matriks koefisien
+x dinamakan matriks peubah
+b dinamakan matriks konstansa
+
+
+jenis solusi spl
+
+- SPL dengan solusi tunggal
+- SPL tidak memiliki solusi
+- SPL dengan solusi tak hingga banyak
+
+Solusi SOL dengan matriks inversi bisa dinotasikan dengan 
+X = A^-1 (invers) * B
+
+A inversi bisa dicari dengan metode OBE
+
+'''
+
+
