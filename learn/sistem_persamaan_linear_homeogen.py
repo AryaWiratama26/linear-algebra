5f823c9593f3072555a1c66a972310572f916e11
Arya Wiratama <aryawiratama2401@gmail.com>
2025-08-10 20:53:25 +0700

    udpate: sistem persamaan linear homogen

---
+++ learn/sistem_persamaan_linear_homeogen.py
@@ -0,0 +1,34 @@
+"""
+
+
+AX = 0
+
+Jenis-jenis spl homogen
+
+1. Solusi Trivial : Jika solusi adalah tunggal yaitu X1 = 0, X2 = 0, Xn = 0
+2. Solusi Non-Trivial : Jika Solusi lain selain nol
+
+Tentunkan solusi dari SPL homogen berikut
+
+{x1 + x2 = 0}
+{x1 - x2 = 0}
+{2x2 + x3 = 0}
+
+
+Bentuk matriks koefisien
+
+[1 0  1](x1) = (0)
+[1 -1 0](x2) = (0)
+[0 2 1](x3) = (0)
+
+lalu bentuk menjadi matriks identitas dengan metode OBE
+
+hasilnya adalah
+
+[1 0 0](x1) = (0)
+[0 1 0](x2) = (0)
+[0 0 1](x3) = (0)
+
+
+
+"""
